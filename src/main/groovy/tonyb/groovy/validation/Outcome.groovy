ef63ff4c8c6f39de53db2ccc681d73fbed4bdd8a
tony <tony@count-zero>
2010-05-08 15:59:21 +0100

    Sort function for outcomes

--- src/main/groovy/tonyb/groovy/validation/Outcome.groovy
+++ src/main/groovy/tonyb/groovy/validation/Outcome.groovy
@@ -9,6 +9,8 @@ package tonyb.groovy.validation
     static def serious(msg) { new Outcome(Level.Serious, msg) }
     static def processingError(msg) { new Outcome(Level.ProcessingError, msg) }
 
+    static def sort(outcomes) { outcomes.sort { a,b -> a.level <=> b.level } }
+
     // Useful predefined filters
     static def trivialities(outcomes) { outcomes.findAll {it.level == Level.Trivial} }
     static def warnings(outcomes) { outcomes.findAll {it.level == Level.Warning} }
