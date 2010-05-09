36fc71dee1488f8e06c9d50535eb72bc9ddfc1a9
tony <tony@count-zero>
2010-05-09 16:51:07 +0100

    Multiple context objects allowed in validations, some helper methods to get around awkward syntax

--- src/main/groovy/tonyb/groovy/validation/Rule.groovy
+++ src/main/groovy/tonyb/groovy/validation/Rule.groovy
@@ -5,11 +5,25 @@ package tonyb.groovy.validation
     public static rule(Closure test) {
         new Rule(test)
     }
-    def Outcome examine(context) {
-      try {
-          test.call(context)
-      } catch (Throwable e) {
-          Outcome.processingError e.message
-      }
+    def Outcome examine(Object context) { tryToCall {test(context)} }
+
+    // Convenience methods for more than one context object
+    def Outcome examine(a,b) { tryToCall { test(a,b) } }
+    def Outcome examine(a,b,c) { tryToCall { test(a,b,c) } }
+    def Outcome examine(a,b,c,d) { tryToCall { test(a,b,c,d) } }
+    def Outcome examine(a,b,c,d,e) { tryToCall { test(a,b,c,d,e) } }
+
+    // More arguments need to be treated as an array
+    def Outcome examine(Object[] context) { tryToCall {test(context) } }
+
+    /*
+     * Execute the supplied closure, trapping any exceptions
+     */
+    private def tryToCall(Closure c) {
+        try {
+            c.call()
+        } catch (Throwable e) {
+            Outcome.processingError e.message
+        }
     }
 }
