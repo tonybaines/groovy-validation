025a7a4c2e491d2af75e35fa04299bf4defc3bff
tony <tony@count-zero>
2010-05-09 14:12:43 +0100

    Fix for pipelines of filters Reduced duplication in filter definitions

--- src/main/groovy/tonyb/groovy/validation/OutcomeHelper.groovy
+++ src/main/groovy/tonyb/groovy/validation/OutcomeHelper.groovy
@@ -2,34 +2,49 @@ package tonyb.groovy.validation;
 
 @Category(ArrayList)
 class OutcomeHelper {
-    static ArrayList<Outcome> getSorted(ArrayList<Outcome> outcomes) {
-        outcomes.sort { a,b -> a.level <=> b.level }
+
+    static def getSorted(ArrayList<Outcome> outcomes) {
+        mixinSelf { outcomes.sort { a,b -> a.level <=> b.level } }
     }
 
-    static ArrayList<Outcome> getOnlyInformation(ArrayList<Outcome> outcomes) {
-        outcomes.findAll {it.level == Level.Information}
+    static def getOnlyInformation(ArrayList<Outcome> outcomes) {
+        filterByLevel(outcomes, Level.Information)
+    }
+    static def getOnlyWarnings(ArrayList<Outcome> outcomes) {
+        filterByLevel(outcomes, Level.Warning)
+    }
+    static def getOnlySerious(ArrayList<Outcome> outcomes) {
+        filterByLevel(outcomes, Level.Serious)
+    }
+    static def getOnlyProcessingErrors(ArrayList<Outcome> outcomes) {
+        filterByLevel(outcomes, Level.ProcessingError)
     }
 
-    static ArrayList<Outcome> getOnlyWarnings(ArrayList<Outcome> outcomes) {
-        outcomes.findAll {it.level == Level.Warning}
+    static def getLessThanSerious(ArrayList<Outcome> outcomes) {
+        findAllBy outcomes, {it.level < Level.Serious}
+    }
+    static def getMoreThanInformation(ArrayList<Outcome> outcomes) {
+        findAllBy outcomes, {it.level > Level.Information}
+    }
+    static def getIgnoringProcessingErrors(ArrayList<Outcome> outcomes) {
+        findAllBy outcomes, {it.level != Level.ProcessingError}
     }
 
-    static ArrayList<Outcome> getOnlySerious(ArrayList<Outcome> outcomes) {
-        outcomes.findAll {it.level == Level.Serious}
+
+
+    private static def filterByLevel(outcomes, level) {
+        findAllBy outcomes, {it.level == level}
+    }
+    private static def findAllBy(outcomes, Closure criterium) {
+        mixinSelf { outcomes.findAll(criterium) }
     }
 
-    static ArrayList<Outcome> getOnlyProcessingErrors(ArrayList<Outcome> outcomes) {
-        outcomes.findAll {it.level == Level.ProcessingError} }
-
-    static ArrayList<Outcome> getLessThanSerious(ArrayList<Outcome> outcomes) {
-        outcomes.findAll {it.level < Level.Serious}
-    }
-
-    static ArrayList<Outcome> getMoreThanInformation(ArrayList<Outcome> outcomes) {
-        outcomes.findAll {it.level > Level.Information}
-    }
-
-    static ArrayList<Outcome> getIgnoringProcessingErrors(ArrayList<Outcome> outcomes) {
-        outcomes.findAll {it.level != Level.ProcessingError}
+    /*
+     * Necessary to allow pipelines of filters
+     */
+    private static mixinSelf(Closure f) {
+        def result = f.call()
+        result.metaClass.mixin OutcomeHelper
+        result
     }
 }
